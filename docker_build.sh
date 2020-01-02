14fbef1a963de65cc432787e78310978653f41c6
Andrey Tovchigrechko <andrey.tovchigrechko@astrazeneca.com>
2020-01-02 18:17:06 -0500

    Update Docker build name and tags to point to Dockerhub

--- docker_build.sh
+++ docker_build.sh
@@ -10,8 +10,12 @@ set -ex
 
 mkdir -p build_data_files/test_data/rsv
 cp -a ../ngs-mstb-test-data/micgent_large_test_data/rsv/INF2019_CTR build_data_files/test_data/rsv/
+
 docker build \
 --tag ${NGS_MSTB_DOCKER_IMAGE}:${NGS_MSTB_DOCKER_TAG} \
 --build-arg GALAXY_UID_NEW=$GALAXY_UID_NEW \
 --build-arg TOOL_CONF_BASE=tool_conf_no_radar.xml \
 --build-arg GALAXY_GID_NEW=$GALAXY_GID_NEW .
+
+docker tag ${NGS_MSTB_DOCKER_IMAGE}:${NGS_MSTB_DOCKER_TAG} ${NGS_MSTB_DOCKER_IMAGE}:latest
+
--- docker_common.sh
+++ docker_common.sh
@@ -1,3 +1,3 @@
-export NGS_MSTB_DOCKER_IMAGE=ngs-mstb
+export NGS_MSTB_DOCKER_IMAGE=ngsmstb/ngs-mstb
 export NGS_MSTB_DOCKER_TAG=2.5
 export NGS_MSTB_DOCKER_DEPS_IMAGE=ngs-mstb-deps
